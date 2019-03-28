902d4d071766f329e8e6b13102fb07595ddfcb0b
Anton <ttropin@gmail.com>
2019-03-28 15:04:16 +0300

    Связал таблицы, заполнил, создали несколько продуктов.

--- server/productsapp/views.py
+++ server/productsapp/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from .models import Category, Type, Product
 
 # Create your views here.
 
-def product_list(requiest):
-    return render(requiest, 'productsapp/product_list.html')
+def product_list(requiest, pk):
+    return render(
+        requiest, 
+        'productsapp/product_list.html',
+        {
+            'categorys' : Category.objects.get(id=pk), 
+            'types' : Type.objects.get(category=pk),
+        }
+    )
 
 def product_detail(requiest):
     return render(requiest, 'productsapp/product_detail.html')
